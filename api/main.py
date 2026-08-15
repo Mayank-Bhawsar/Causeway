dd6d32d608b25b7e0acd5a3b049b5cdea590a4d3
Mayank-Bhawsar <mayankbhawsar1@gmail.com>
2026-08-15 12:42:34 +0000

    adding ingest_router to main.py

--- api/main.py
+++ api/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 
+
+
 from api.routes.health import router as health_router
+from api.routes.ingest import router as ingest_router
 
 app = FastAPI(title="Causeway", version="0.1.0")
 app.include_router(health_router)
+app.include_router(ingest_router)
